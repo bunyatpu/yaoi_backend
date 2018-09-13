651a831393345ba8286902e23d68e2aae17c7fa1
bunyut <bunyut.p@gmail.com>
2018-09-14 03:24:30 +0700

    upate

--- config/db/db.config.js
+++ config/db/db.config.js
@@ -2,10 +2,14 @@
  * Created by adithep on 10/20/2017 AD.
  */
 
+var dbUrl = (process.env.db_url !== undefined) ? process.env.db_url:"db";
+var user = (process.env.db_url !== undefined) ? "snuffle":"root";
+var pass = (process.env.db_url !== undefined) ? "jjtomson":"123456";
+
 module.exports = {
-  cfg_db_host: "db",
-  cfg_db_user: "root",
-  cfg_db_pwd: "123456",
+  cfg_db_host: dbUrl,
+  cfg_db_user: user,
+  cfg_db_pwd: pass,
   cfg_MAX_POOL: 10,
   cfg_MIN_POOL: 0,
   cfg_IDLE: 10000,
